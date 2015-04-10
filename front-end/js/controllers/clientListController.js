20cef2cc18465676fd7aab3ae28e4b3851b923e2
Renan JP <renanjohannsen@hotmail.com>
2015-04-10 14:09:44 -0300

    added $location

--- front-end/js/controllers/clientListController.js
+++ front-end/js/controllers/clientListController.js
@@ -1,20 +1,28 @@
 'use strict';
 
-angular.module('app').controller('ClientListController', function($scope, $log, clientService) {
+angular.module('app').controller('ClientListController', function($scope, $location, $log, clientService) {
 
 	$scope.init = function() {
-		$scope.totalItems = 1;
-		$scope.currentPage = 1;
-		$scope.maxSize = 5;
-
-		$scope.clients = clientService.getClients();
+		var query = $location.search();
+		$scope.clients = clientService.getClients(query);
+		$scope.searchParans = {};
+		$scope.searchParans.name = query.name;
+		$scope.searchParans.collectionSize = $scope.clients.length;
+		$scope.searchParans.currentPage = query.page ? query.page : 1;
+		$scope.searchParans.paginatedIn = 5;
 	};
 
 	$scope.setPage = function (pageNumber) {
-		$scope.currentPage = pageNumber;
+		$scope.searchParans.currentPage = pageNumber;
 	};
 
-	$scope.pageChanged = function() {
-		$log.log('Page changed to: ' + $scope.currentPage);
+	$scope.pageChanged = function () {
+		$log.log('Page changed to: ' + $scope.searchParans.currentPage);
+		$location.path('/clients').search( {'name' : $scope.searchParans.name, 'page' : $scope.searchParans.currentPage } );
 	};
+
+	$scope.search = function () {
+		$location.path('/clients').search( {'name' : $scope.searchParans.name, 'page' : 1 } );
+	}
+
 });
--- front-end/js/services/clientService.js
+++ front-end/js/services/clientService.js
@@ -2,17 +2,60 @@
 
 angular.module('app').factory('clientService', function($http, $log, serverPath) {
 
-	function getClients() {
+	var clients = [
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Renan', 'workOrders' : 1 },
+		{ 'id' : 1, 'name' : 'Rodolfo', 'workOrders' : 2 }
+	]; 
+	var countId = 0;
+
+	function getClients(search) {
 		$log.log('Get in /Clients');
-		return [ 
-			{ 'id' : 10, 'name' : 'Renan Johannsen de Paula', 'workOrders' : 150 },
-			{ 'id' : 10, 'name' : 'Renan Johannsen de Paula', 'workOrders' : 150 },
-			{ 'id' : 10, 'name' : 'Renan Johannsen de Paula', 'workOrders' : 150 },
-			{ 'id' : 10, 'name' : 'Renan Johannsen de Paula', 'workOrders' : 150 }
-		];
+		var clientsSelected = [];
+		if (!search.name) {
+			clientsSelected = clients;
+		} else {
+			for (var i = 0; i < clients.length; i++) {
+				var client = clients[i];
+				if(client.name.search(search.name) != -1) {
+					clientsSelected.push(client);
+				}
+			}
+		}
+		return clientsSelected;
+	}
+
+	function getClient(clientId) {
+		for (var i = 0; i < clients.length; i++) {
+			var client = clients[i];
+			if(client == clientId) {
+				return client;
+			}
+		};
+		return null;
+	}
+
+	function saveClient(client) {
+		if(!client.id) {
+			$log.log('Post in /Clients');
+			client.id = ++countId;
+		} else {
+			$log.log('Put in /Clients');
+		}
+		clients.put(client);
 	}
 
 	return {
-		'getClients': getClients
+		'getClient'  : getClient,
+		'getClients' : getClients,
+		'saveClient' : saveClient
 	};
 });
